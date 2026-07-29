08a0c78f7aac9c7a34d7cd979fcfaf57ef71980f
charis panayides <127757926+panachar1997@users.noreply.github.com>
2026-07-29 13:45:59 +0300

    v29

--- vite.pages.config.ts
+++ vite.pages.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: "/charis-editorial-showcase/",
+  base: "/web-portfolio/",
   plugins: [
     tsConfigPaths(),
     tailwindcss(),
